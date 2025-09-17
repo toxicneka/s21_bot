c6bd4925e42b38407098d431f82dd6a498002395
Corrinne Jonathan <toxicneka@gmail.com>
2025-09-17 18:25:02 +0900

    nice

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -319,11 +319,9 @@ async def get_access_token(login_token: str, password_token: str) -> str:
 
 async def get_cluster_info(cluster_id: str, token: str) -> dict:
     url = f"https://platform.21-school.ru/services/21-school/api/v1/clusters/{cluster_id}/map?limit=100&offset=0"
-    print(cluster_id)
     headers = {'Authorization': f'Bearer {token}'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        print("check platform")
         return response.json()
     return None
 
