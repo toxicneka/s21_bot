8c5e3da840c2772a92fe730f5f66c7ee2a05555a
Corrinne Jonathan <toxicneka@gmail.com>
2026-01-21 17:12:23 +0900

    fix

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 MAIN_ADMIN_ID = os.getenv("MAIN_ADMIN_ID")
-login_token = os.getenv("login_token")
+login_token = os.getenv("login_token") #school
 password_token = os.getenv("password_token")
 GOOGLE_SHEETS_CREDS = os.getenv("GOOGLE_SHEETS_CREDS")
 SPREADSHEET_KEY = os.getenv("SPREADSHEET_KEY")
